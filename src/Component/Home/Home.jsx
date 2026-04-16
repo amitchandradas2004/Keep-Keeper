48663ef33c5f9e862db6365191b49211aa9b7ec2
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-16 16:07:49 +0600

    Hero is added

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./Hero";
+import CardLode from "../CardLode/CardLode";
+import AllCard from "../ShowCard/AllCard";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero></Hero>
+      <CardLode></CardLode>
+    </div>
+  );
+};
+
+export default Home;
